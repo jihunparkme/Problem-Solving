d7b71d7212f8625d9ba21dcf50b35cc24660230d
jihunparkme <jihunpark.tech@gmail.com>
2020-06-08 22:25:00 +0900

    BOJ PS solved

---
+++ PS_Study/2523.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(void)
+{
+	int i, j, n, m = 1;
+	scanf("%d", &n);
+
+	for (i = 1; i > 0; i += m) {
+		for (j = 1; j <= i; j++)
+			printf("*");
+
+		puts("");
+		if (i >= n) m *= -1;
+	}
+		
+	return 0;
+}
