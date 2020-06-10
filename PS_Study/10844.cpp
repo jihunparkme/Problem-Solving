7ea62c1a1fab896d019b0d4f6df0d424b7391e1f
jihunparkme <jihunpark.tech@gmail.com>
2020-06-10 21:48:14 +0900

    BOJ PS solved

---
+++ PS_Study/10844.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+int dp[101][11];
+
+int main(void)
+{
+	int n, i, j;
+	long long res = 0;
+	scanf("%d", &n);
+
+	for (i = 1; i <= 9; i++)
+		dp[1][i] = 1;
+
+	for (i = 2; i <= n; i++) {
+		for (j = 0; j <= 9; j++) {
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+		}
+	}
+
+	for (i = 0; i <= 9; i++) 
+		res += dp[n][i];
+
+	printf("%lld\n", res % 1000000000);
+	return 0;
+}
