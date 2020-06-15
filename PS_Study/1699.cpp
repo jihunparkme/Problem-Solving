1f0a7568792d6ec010f53847003e798d213beec7
jihunparkme <jihunpark.tech@gmail.com>
2020-06-15 14:48:08 +0900

    BOJ PS solved

--- PS_Study/1699.cpp
+++ PS_Study/1699.cpp
@@ -1,24 +1,18 @@
 #include <cstdio>
-
 int min(int p, int q) { return p > q ? q : p; }
-
 int DP[100001];
-
 int main(void)
 {
 	int n, i, j;
 	// freopen("input.txt", "rt", stdin);
 	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= n; i++) {
 		DP[i] = i;
-
-	for (i = 1; i*i <= n; i++) {
-		for (j = i*i; j <= n; j++) {
-			DP[j] = min(DP[j], DP[j - i * i] + 1);
+		for (j = 1; j * j <= i; j++) {
+			DP[i] = min(DP[i], DP[i - j * j] + 1);
 		}
 	}
 
 	printf("%d\n", DP[n]);
-
 	return 0;
 }
