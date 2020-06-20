cf170e26fb28db3f95510bc92228506e6786477f
jihunparkme <pjh073154@gmail.com>
2020-06-20 23:19:23 +0900

    BOJ PS solved

--- PS_Study/9461.cpp
+++ PS_Study/9461.cpp
@@ -7,13 +7,14 @@ int main(void)
 	// freopen("input.txt", "rt", stdin);
 
 	for (i = 7; i <= 100; i++) 
-		DP[i] = DP[i - 3] + DP[i - 2];
+		DP[i] = DP[i - 3] + DP[i - 2]; 
 
 	scanf("%d", &tc);
 	while (tc--) {
 		scanf("%d", &n);
 		printf("%lld\n", DP[n]);
 	}
+
 
 	return 0;
 }
