0962fe8a9a6bae8a63fad50600e0ca82dd46d0be
jihunparkme <pjh073154@gmail.com>
2020-06-24 15:12:51 +0900

    BOJ PS solved

--- PS_Study/1260.cpp
+++ PS_Study/1260.cpp
@@ -5,39 +5,41 @@
 using namespace std;
 
 int N, M, V;	// 정점 개수, 간선 개수
-vector<int> ch;
+vector<int> ck;
 vector<vector<int> > vt;
 
 void DFS(int s)
 {
 	int i;
-	if (ch[s]) return;
-	
-	ch[s] = 1;
 	printf("%d ", s);
 	for (i = 0; i < vt[s].size(); i++) {
-		DFS(vt[s][i]);
+		if (!ck[vt[s][i]]) {
+			ck[vt[s][i]] = 1;
+			DFS(vt[s][i]);
+		}
 	}
 }
 
 void BFS(int s)
 {
 	int i;
-	vector<int> ch2(N + 1);
+	vector<int> ck2(N + 1);
 	queue<int> q;
 
 	q.push(V);
+	ck2[V] = 1;
 	while (!q.empty()) {
 		int x = q.front();
 		q.pop();
-		if (!ch2[x])
-			printf("%d ", x);
-		ch2[x] = 1;
-
+	
 		for (i = 0; i < vt[x].size(); i++) {
-			if (!ch2[vt[x][i]])
+			if (!ck2[vt[x][i]]) {
 				q.push(vt[x][i]);
+				ck2[vt[x][i]] = 1;
+			}
 		}
+
+		printf("%d ", x);
 	}
 }
 
@@ -58,7 +60,8 @@ int main(void)
 		sort(vt[i].begin(), vt[i].end());
 	
 	// DFS
-	ch.resize(N+ 1, 0);
+	ck.resize(N+ 1, 0);
+	ck[V] = 1;
 	DFS(V);
 
 	puts("");
