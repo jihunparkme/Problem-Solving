58a75ae1fc76f7da232fdd41743e712604d1f80b
jihunparkme <pjh073154@gmail.com>
2020-07-12 00:05:38 +0900

    BOJ PS solved

---
+++ PS_Study/1991.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#define x first
+#define y second
+#define MAX 2147000000
+#define MIN -2147000000
+int max(int p, int q) { return p > q ? p : q; }
+
+int map[54];
+
+int main(void)
+{
+	freopen("input.txt", "rt", stdin);
+	int n, i;
+	scanf("%d", &n);
+	for (i = 1; i < n; i++) {
+
+	}
+
+	return 0;
+}
